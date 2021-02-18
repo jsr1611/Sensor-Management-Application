5f87ce516cb311170f1a4a441fc1f8a99852c0e1
Jumanazar Saidov <jsr1611@outlook.com>
2021-02-18 18:34:54 +0900

    Final revision 6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sensor Management Admin Page Application")]
+[assembly: AssemblyTitle("Admin Page Application")]
 [assembly: AssemblyDescription("Sensor Management Admin Page for Data Collection and Visualization Application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DLIT Inc.")]
